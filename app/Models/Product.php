50143168544d66e52180fef4a463a9cb322b3a45
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2024-02-14 15:49:55 +0200

    MODEL UPDATES

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\Company;
 use App\Models\Brand;
+use App\Models\Company;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -18,7 +19,12 @@ class Product extends Model
         'company_id',
         'brand_id',
     ];
-
+    public static function getCountOfSchema(){
+        return DB::table('INFORMATION_SCHEMA_TABLES')
+        ->where('TABLE_SCHEMA', DB::connection()->getDatabaseName())
+        ->where('TABLE_NAME', static::make()->getTable())
+        ->first()->TABLE_ROWS;
+    }
     public function company(){
         return $this->belongsTo(Company::class);
     }
